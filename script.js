d2cf9c38734ac5e791828f6c91637cc886f64018
endx-star <endalework90@gmail.com>
2025-07-02 11:11:36 +0300

    add a map marker

--- script.js
+++ script.js
@@ -13,10 +13,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+map.on('click', function(mapEvent) {
+
+  const {lat, lng} = mapEvent.latlng;
+  L.marker([lat, lng]).addTo(map)
+    .bindPopup(
+      L.popup({
+           maxWidth: 250,
+           minWidth: 100,
+           autoClose: false,
+           closeOnClick: false,
+           className: 'running-popup'
+    })).setPopupContent('Workout')
     .openPopup();
+})
+
 },
+
 
 function (){
   alert("cound't access the coordinates");
